cdbcecdc18db92d270bbd18322e25c31c060a4aa
kelleyindahouse <kelleyindahouse@gmail.com>
2021-11-19 15:32:19 -0500

    deleted some risidual code

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
-var options = {
-  method: 'GET',
-  url: 'https://dad-jokes.p.rapidapi.com/random/joke',
-  headers: {
-    'x-rapidapi-host': 'dad-jokes.p.rapidapi.com',
-    'x-rapidapi-key': '49064f2772msh340d638e8821a73p1847f5jsn3c12c7ed68c0'
-  }
-};
 
-axios.request(options).then(function (response) {
-  console.log(response.data);
-}).catch(function (error) {
-  console.error(error);
-});
 
