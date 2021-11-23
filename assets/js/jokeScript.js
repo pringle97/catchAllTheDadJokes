e33ea055a3068e34a4d821a00495d2f007e86a12
kelleyindahouse <kelleyindahouse@gmail.com>
2021-11-23 17:30:19 -0500

    added third quote api

--- assets/js/jokeScript.js
+++ assets/js/jokeScript.js
@@ -15,8 +15,13 @@
 document.getElementById('random').addEventListener('click', event => {
   event.preventDefault();
 
-  let randomNumber = Math.floor(Math.random() * 2);
+  let randomNumber = Math.floor(Math.random() * 3);
   console.log(randomNumber);
+  const quoteElem = document.getElementById('quote');
+  const setupElem = document.getElementById('setup');
+  const jokeElem = document.getElementById('joke');
+  let i = 0;
+  let speed = 10;
   
   
   if (randomNumber === 0 ) {
@@ -24,24 +29,15 @@ document.getElementById('random').addEventListener('click', event => {
     axios.get(`https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes`)
     .then(res => {
       console.log(res);
-        const quoteElem = document.getElementById('quote');
-        const setupElem = document.getElementById('setup');
-        const punchlineElem = document.getElementById('punchline');
+        
         let punchline = res.data.punchline;
         let setup = res.data.setup;
         let words = `${setup} - ${punchline}`
-        let i = 0;
-        let speed = 30;
-        
-        
-        console.log(setup)
-        console.log(setup.charAt(i))
-        console.log(words)
         
         setupElem.innerHTML = '';
         quoteElem.innerHTML = '';
-        punchlineElem.innerHTML = '';
-
+        jokeElem.innerHTML = '';
+      
         function typeWriter() {
           if (i < words.length) {
             document.getElementById("setup").innerHTML += words.charAt(i);
@@ -51,46 +47,44 @@ document.getElementById('random').addEventListener('click', event => {
         }
 
         typeWriter()
-        
-        
-        // function typeWriter2() {
-        //   if (i < punchline.length) {
-        //     document.getElementById("punchline").innerHTML += punchline.charAt(i);
-        //     i++;
-        //     setTimeout(typeWriter2, speed);
-        //   }
-        // }
-        
-        // console.log(punchline)
-        // console.log()
-        
-        
-        
-        
-        // typeWriter();
-        // typeWriter2();
-    
-        // quoteElem.innerHTML = `
-        //   <p>${setup}</p>
-        //   <p>${punchline}</p>
-        //   `;     
+       
     })
   // .catch(err => console.log(err));
 
+  } else if (randomNumber === 1) {
+    // axios get for other joke
+    axios.get(`https://goquotes-api.herokuapp.com/api/v1/random?count=1`)
+      .then(res => {
+        console.log(res)
+        let quote = res.data.quotes[0].text     
+
+        quoteElem.innerHTML = '';
+        setupElem.innerHTML = '';
+        jokeElem.innerHTML = '';
+
+        console.log(quote)
+
+        function typeWriter2() {
+          if (i < quote.length) {
+            document.getElementById("quote").innerHTML += quote.charAt(i);
+            i++;
+            setTimeout(typeWriter2, speed);
+          }
+        }
+
+        typeWriter2()
+
+      })
+      .catch(err => console.log(err))
   } else {
     axios.get(`https://geek-jokes.sameerkumar.website/api?format=json`)
       .then(res => {
         console.log(res);
         let joke = res.data.joke;
-        const quoteElem = document.getElementById('quote');
-        const setupElem = document.getElementById('setup');
-        const punchlineElem = document.getElementById('punchline');
-        let i = 0;
-        let speed = 30;
         
         quoteElem.innerHTML = '';
         setupElem.innerHTML = '';
-        punchlineElem.innerHTML = '';
+        jokeElem.innerHTML = '';
 
         function typeWriter3() {
           if (i < joke.length) {
@@ -99,14 +93,11 @@ document.getElementById('random').addEventListener('click', event => {
             setTimeout(typeWriter3, speed);
           }
         }
-        // console.log(joke)
-        // console.log(joke.charAt(i))
+        
         typeWriter3();
-
-                
+         
       })
       .catch(err => console.log(err))
-      
   }
 
 })
