9cd8fa7e7158cb3882ca244cbcc00d0074ff8bd8
kelleyindahouse <kelleyindahouse@gmail.com>
2021-11-22 20:41:27 -0500

    added assets folder and moved files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,16 @@
 // const pokemonArr = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran", "Nidorina", "Nidoqueen", "Nidoran", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetch'd", "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggtor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie", "Mr. Mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo", "Mew"]
-let pokemonNum = (Math.floor(Math.random() * 151) + 1)
-console.log(pokemonNum)
 
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.modal');
-  var instances = M.Modal.init(elems);
-});
+
+let randomNumber = (Math.floor(Math.random()))
+
 //generating number between 0 and 1 
 let x = Math.floor((Math.random() * 2));
 let ball = document.getElementById('ball')
 let berry = document.getElementById('berry')
 let goNear = document.getElementById('go-near')
 let runAway = document.getElementById('run-away')
+console.log(x)
+
 let capitalize = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
@@ -20,46 +19,59 @@ let addDecimal = (num) => {
   return (num / 10).toFixed(1);
 }
 
-console.log(x)
+// function for mcss modals
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems);
+});
+
+// function for mcss scrollspy
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.scrollspy');
-  var instances = M.ScrollSpy.init(elems, options);
+  var instances = M.ScrollSpy.init(elems);
 });
-var instance = M.ScrollSpy.getInstance(elem);
-document.getElementById('start-button').addEventListener('click', event => {
-  event.preventDefault()
 
+// function for mcss sidenav
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+
+document.getElementById('start-button').addEventListener('click', event => {
+  event.preventDefault();
+  let pokemonNum = (Math.floor(Math.random() * 151) + 1)
+  console.log(pokemonNum)
+  
+  document.getElementById('pokemon').innerHTML = '';
 
   axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonNum}`)
     .then(res => {
-      const pokemon = res.data
-      console.log(pokemon)
+      const pokemon = res.data;
+      console.log(pokemon);
 
       if (pokemon.types[1]) {
-        document.getElementById('pokemon').
-        innerHTML = `
+        document.getElementById('pokemon').innerHTML = `
+        <img src = "${pokemon.sprites.front_default}" alt="${pokemon.species.name}" width="100%">
         <h1>Pokemon Name: ${capitalize(pokemon.species.name)}</h1>
         <h2>Type: ${capitalize(pokemon.types[0].type.name)}, ${capitalize(pokemon.types[1].type.name)}</h2 >
         <h5>Height: ${addDecimal(pokemon.height)} m</h5>
         <h5>Weight: ${addDecimal(pokemon.weight)} kg</h5>
-        <img src = "${pokemon.sprites.front_default}" alt="${pokemon.species.name}">
-      `
+        `;
       } else {
-        console.log(pokemon)
-        console.log(pokemon.types[0].type.name)
-      document.getElementById('pokemon').
-      innerHTML = `
+        console.log(pokemon);
+        console.log(pokemon.types[0].type.name);
+        document.getElementById('pokemon').innerHTML = `
+        <img src = "${pokemon.sprites.front_default}" alt="${pokemon.species.name}" width="100%">
         <h1>Pokemon Name: ${capitalize(pokemon.species.name)}</h1>
         <h2>Type: ${capitalize(pokemon.types[0].type.name)}</h2 >
         <h5>Height: ${addDecimal(pokemon.height)} m</h5>
         <h5>Weight: ${addDecimal(pokemon.weight)} kg</h5>
-        <img src = "${pokemon.sprites.front_default}" alt="${pokemon.species.name}">
-      `
+        `;
       }
-    
     })
-    .catch(err => console.log(err))
-})
+  // .catch (err => console.log(err))
+});
 
 
 
@@ -91,4 +103,4 @@ document.getElementById('start-button').addEventListener('click', event => {
 //     })
 //     .catch(err => console.log(err))
 // })
-let randomNumber = (Math.floor(Math.random()))
+
