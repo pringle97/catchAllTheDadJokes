644c0f48987aac5ca3c0aedbb26109739c0b3838
Peter Song <psong562@gmnail.com>
2021-11-27 19:22:48 -0800

    added about us html/css/js

---
+++ about.js
@@ -0,0 +1,54 @@
+const interactElem = document.getElementById(`interact`)
+
+
+document.getElementById(`alan`).addEventListener(`click`, () => {
+  document.getElementById(`name-screen`).innerHTML =`Alan`
+  document.getElementById(`about-screen`).innerHTML =`Height: 1.65m Weight: 58.97kg`
+  document.getElementById(`type-screen`).innerHTML =`Psychic`
+  document.getElementById(`id-screen`).innerHTML =`1`
+})
+document.getElementById(`kevin`).addEventListener(`click`, () => {
+  document.getElementById(`name-screen`).innerHTML =`Kevin`
+  document.getElementById(`about-screen`).innerHTML =`Height: 1.80m Weight: 64.86kg`
+  document.getElementById(`type-screen`).innerHTML =`Normal`
+  document.getElementById(`id-screen`).innerHTML =`2`
+})
+document.getElementById(`malia`).addEventListener(`click`, () => {
+  document.getElementById(`name-screen`).innerHTML =`Malia`
+  document.getElementById(`about-screen`).innerHTML =`Height: 1.63m Weight: 70.3kg`
+  document.getElementById(`type-screen`).innerHTML =`Water`
+  document.getElementById(`id-screen`).innerHTML =`3`
+})
+document.getElementById(`nathan`).addEventListener(`click`, () => {
+  document.getElementById(`name-screen`).innerHTML =`Nathan`
+  document.getElementById(`about-screen`).innerHTML =`Height: 1.83m Weight: 70.3kg`
+  document.getElementById(`type-screen`).innerHTML =`Grass/Rock`
+  document.getElementById(`id-screen`).innerHTML =`4`
+})
+document.getElementById(`peter`).addEventListener(`click`, () => {
+  document.getElementById(`name-screen`).innerHTML =`Peter Song`
+  document.getElementById(`about-screen`).innerHTML =`Height: 1.72m Weight: 65.77kg`
+  document.getElementById(`type-screen`).innerHTML =`Rock`
+  document.getElementById(`id-screen`).innerHTML =`5`
+ 
+  
+
+  let i = 0;
+  let speed = 10;
+  let words = `Hey! My name is Peter Song. My favorite hobbies are sports activites(Basketball, Football and Badminton) and video games(Currently League of Legends and Destiny 2).`
+  interactElem.innerHTML = ``
+
+  function typeWriter() {
+    if (i < words.length) {
+      document.getElementById(`interact`).innerHTML += words.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+
+  typeWriter()
+
+
+})
+
+
